a6f4d34e04efa6955a20b7afd431de883f51c5d0
Corey <corey@pop-os.localdomain>
2024-06-25 00:29:07 -0600

    add main header file

---
+++ linalg.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <limits>
+#include <cassert>
+#include "types.hpp"
+
+#include "vectors/vector2.hpp"
+#include "vectors/vector3.hpp"
+#include "vectors/vector4.hpp"
+
+//#include "matrices/incl_matrices.hpp"
+
+#include "func/geometric.hpp"
